79aebd0b1e377a5dcb9b6d2053af3ec2391f0ac9
Atul <atulchavan528@gmail.com>
2023-07-06 23:24:50 +0530

    custom and global exception handling

--- employee-service/src/main/java/net/employee/management/employeeservice/controller/EmployeeController.java
+++ employee-service/src/main/java/net/employee/management/employeeservice/controller/EmployeeController.java
@@ -1,11 +1,18 @@
 package net.employee.management.employeeservice.controller;
 
+import jakarta.annotation.Resource;
 import lombok.AllArgsConstructor;
 import net.employee.management.employeeservice.dto.EmployeeDto;
+import net.employee.management.employeeservice.exception.ErrorDetails;
+import net.employee.management.employeeservice.exception.ResourceNotFoundException;
 import net.employee.management.employeeservice.service.EmployeeService;
+import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
 
 @RestController
 @RequestMapping("/api/employees")
@@ -27,4 +34,19 @@ public class EmployeeController {
         EmployeeDto employeeDto = employeeService.getEmployeeById(employeeId);
         return new ResponseEntity<>(employeeDto, HttpStatus.OK);
     }
+
+
+
+//    @ExceptionHandler(ResourceNotFoundException.class)
+//    public ResponseEntity<ErrorDetails> handleResourceNotFoundException(
+//            ResourceNotFoundException exception,
+//            WebRequest webRequest) {
+//            ErrorDetails errorDetails = new ErrorDetails(
+//                LocalDateTime.now(),
+//                exception.getMessage(),
+//                webRequest.getDescription(false),
+//                "USER_NOT_FOUND"
+//        );
+//        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+//    }
 }
--- employee-service/src/main/java/net/employee/management/employeeservice/service/impl/EmployeeServiceImpl.java
+++ employee-service/src/main/java/net/employee/management/employeeservice/service/impl/EmployeeServiceImpl.java
@@ -3,6 +3,7 @@ package net.employee.management.employeeservice.service.impl;
 import lombok.AllArgsConstructor;
 import net.employee.management.employeeservice.dto.EmployeeDto;
 import net.employee.management.employeeservice.entity.Employee;
+import net.employee.management.employeeservice.exception.ResourceNotFoundException;
 import net.employee.management.employeeservice.repository.EmployeeRepository;
 import net.employee.management.employeeservice.service.EmployeeService;
 import org.modelmapper.ModelMapper;
@@ -21,9 +22,14 @@ public class EmployeeServiceImpl implements EmployeeService {
         return modelMapper.map(savedEmployee, EmployeeDto.class);
     }
 
+    // orElseThrow method returns Entity
+    // it expects supplier function interface implementation in paramter
+    // therefore we can use lambda expression to implement supplier functional interface
     @Override
     public EmployeeDto getEmployeeById(Long employeeId) {
-        Employee employee = employeeRepository.findById(employeeId).get();
+        Employee employee = employeeRepository.findById(employeeId).orElseThrow(
+                () -> new ResourceNotFoundException("Employee", "id", employeeId)
+        );
         return modelMapper.map(employee, EmployeeDto.class);
     }
 }
