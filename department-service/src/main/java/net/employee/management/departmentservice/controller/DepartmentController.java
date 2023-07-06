242a37a00020ee1037ad6b7b8fac031bb2c6ebf6
Atul <atulchavan528@gmail.com>
2023-07-06 19:06:45 +0530

    depeartment service with two endpoints

---
+++ department-service/src/main/java/net/employee/management/departmentservice/controller/DepartmentController.java
@@ -0,0 +1,29 @@
+package net.employee.management.departmentservice.controller;
+
+import lombok.AllArgsConstructor;
+import net.employee.management.departmentservice.dto.DepartmentDto;
+import net.employee.management.departmentservice.service.DepartmentService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/departments")
+@AllArgsConstructor
+public class DepartmentController {
+    private DepartmentService departmentService;
+
+    // save department api
+    @PostMapping
+    public ResponseEntity<DepartmentDto> saveDepartment(@RequestBody DepartmentDto departmentDto){
+        DepartmentDto savedDepartment = departmentService.saveDepartment(departmentDto);
+        return new ResponseEntity<>(savedDepartment, HttpStatus.CREATED);
+    }
+
+    // get department api
+    @GetMapping("/{departmentCode}")
+    public ResponseEntity<DepartmentDto> getDepartmentByCoode(@PathVariable String departmentCode){
+        DepartmentDto departmentDto = departmentService.getDepartmentByCode(departmentCode);
+        return new ResponseEntity<>(departmentDto, HttpStatus.OK);
+    }
+}
---
+++ department-service/src/main/java/net/employee/management/departmentservice/service/DepartmentService.java
@@ -0,0 +1,10 @@
+package net.employee.management.departmentservice.service;
+
+
+import net.employee.management.departmentservice.dto.DepartmentDto;
+
+public interface DepartmentService {
+    DepartmentDto saveDepartment(DepartmentDto departmentDto);
+
+    DepartmentDto getDepartmentByCode(String code);
+}
